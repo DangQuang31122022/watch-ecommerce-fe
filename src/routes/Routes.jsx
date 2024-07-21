267fe5d57b666966ad6f0c7d03264c981b38862e
dangquang <dangquang@duck.com>
2024-07-21 23:50:32 +0700

    checkout & dashboard

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,8 @@ import Login from "../views/Login";
 import Home from "../views/Home";
 import Account from "../views/account/Account";
 import AccountEdit from "../views/account/AccountEdit";
+import Checkout from "../views/Checkout";
+import Dashboard from "../views/DashBoard";
 
 export default function Routes() {
   return useRoutes([
@@ -26,6 +28,14 @@ export default function Routes() {
       path: "/accountEdit",
       // element: <Navigate to="/account" />,
       element: <AccountEdit />,
+    },
+    {
+      path: "/checkout",
+      element: <Checkout />,
+    },
+    {
+      path: "/admin",
+      element: <Dashboard />,
     },
   ]);
 }
